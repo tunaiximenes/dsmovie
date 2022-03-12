3dac9cbb7b3cb6f55c1e214256e548748a509344
Tunai Ximenes <tunai.ximenes@gmail.com>
2022-03-12 00:06:28 -0300

    First request

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
                     <a href="https://github.com/tunaiximenes">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className='dsmovie-contact-link'>/TMXSolutions</p>
+                            <p className='dsmovie-contact-link'>/TMSolutions</p>
                         </div>
                     </a>
                 </div>
